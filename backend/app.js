100aad81d52fb463e00a7addec924be911276cbf
Biswarup <biswarupghosh520@gmail.com>
2025-03-09 23:50:16 +0530

    added sign up and login

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
+const connectToDb = require("./db/db");
+const userRouter = require("./routes/user.routes");
 
 const app = express();
 
+connectToDb();
+
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+
 app.get("/", (req, res)=>{
     res.send("Hello world");
-})
+});
+app.use("/users",userRouter);
 
 module.exports = app;
---
+++ backend/models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const usersSchema = new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type: String,
+            required: true,
+            minlength: [3, 'First name must be at least 3 characters long']
+        },
+        lastname:{
+            type: String,
+            minlength: [3, 'Last name must be at least 3 characters long']
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength:[5, 'Your email must be at least 5 characters long']
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    socketId: {
+        type: String
+    }
+});
+
+
+usersSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+    return token;
+};
+
+usersSchema.methods.comparePassword = async function(password) {
+    return await bcryptjs.compare(password, this.password)
+};
+
+usersSchema.statics.hashPassword = async function (password) {
+    return await bcryptjs.hash(password, 10);
+};
+
+
+const userModel = mongoose.model("user", usersSchema);
+
+module.exports = userModel;
