16efac72854051da8ee84e2c83968b7d326c8033
Biswarup <biswarupghosh520@gmail.com>
2025-03-17 22:28:03 +0530

    user authentication is done

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const connectToDb = require("./db/db");
 const userRouter = require("./routes/user.routes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -10,6 +10,7 @@ connectToDb();
 
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const blacklistTokenModel = require("../models/blacklistToken.model");
 const userModel = require("../models/user.model");
 const userService = require("../services/user.service");
 const { validationResult } = require("express-validator");
@@ -40,8 +41,24 @@ module.exports.loginUser = async (req, res, next) => {
 
         const token = user.generateAuthToken();
 
+        res.cookie("token", token);
+
         return res.status(200).json({ token, user });
     } catch (error) {
         console.log(error.message)
     }
+}
+
+module.exports.getUserProfile = async (req, res, next) => {
+    return res.status(200).json(req.user);
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+    res.clearCookie("token");
+
+    const token = req.cookies.token || req.headers.authorization.split(" ", [ 1 ]);
+
+    await blacklistTokenModel.create({ token });
+
+    return res.status(200).json({ message: "Logged out successfully" });
 }
